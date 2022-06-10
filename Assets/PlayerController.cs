68b63505c696ed57bc0cabd05806f09c606e7d6a
MatubaraTooru <kurakura.ingen@gmail.com>
2022-06-11 02:23:34 +0900

    砲弾の調整

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,41 +2,33 @@ using UnityEngine;
 
 public class PlayerController: MonoBehaviour
 {
-    //左右移動する力
     [SerializeField] float _moveSpeed = 5f;
     Rigidbody2D _rb = default;
-    //弾丸のプレハブ
     [SerializeField] GameObject _shellPrefab = default;
-    //銃口の位置を設定するオブジェクト
     [SerializeField] Transform _muzzle = default;
-    //水平方向の入力値
+    [SerializeField] float _interval;
     float _h;
-    float _scaleX;
     float _v;
-    //最初に出現した座標
-    Vector3 _initialPosition;
+    float _timer;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        // 初期位置を覚えておく
-        _initialPosition = this.transform.position;
     }
 
     void Update()
     {
-        
-        // 入力を受け取る
         _h = Input.GetAxisRaw("Horizontal");
         _v = Input.GetAxisRaw("Vertical");
-
-        // 各種入力を受け取る
+        _timer += Time.deltaTime;
 
         if (Input.GetButtonDown("Fire1"))
         {
-            Debug.Log("ここに弾を発射する処理を書く。");
-            GameObject go = Instantiate(_shellPrefab);
-            go.transform.position = _muzzle.position;
+            if (_timer > _interval)
+            {
+                Instantiate(_shellPrefab, _muzzle.position, this.transform.rotation);
+                _timer = 0;
+            }
         }
     }
 
--- Assets/ShellController.cs
+++ Assets/ShellController.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class ShellController : MonoBehaviour
 {
     [SerializeField] float m_initialSpeed = 5f;
+    [SerializeField] Transform _crosshair;
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = this.transform.up * m_initialSpeed;
+        Vector2 dir = _crosshair.position - transform.position;
+        transform.up = dir;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = this.transform.up * m_initialSpeed;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
